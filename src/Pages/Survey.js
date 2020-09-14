555f5eb345545d086082e309704501b547a3af6d
Kercho0 <70703656+Kercho0@users.noreply.github.com>
2020-09-14 17:28:08 -0400

    Add files via upload

---
+++ src/Pages/Survey.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+export default class Home extends Component{
+	render(){
+		return(
+			<div className="Survey">
+          	<h1>Daily Survey</h1>
+          	<p> Take the survey before you start your day </p>
+			<iframe title= "Daily Survey" src="https://docs.google.com/forms/d/e/1FAIpQLSeXfOrno_4AgtRt40dwwKcAAntcI4ZFwcfwMFvjA40-z0NhSA/viewform?embedded=true" width="640" height="2312" frameborder="0" marginheight="0" marginwidth="0"></iframe>
+			</div>
+
+			);
+		}
+	}
+
+
