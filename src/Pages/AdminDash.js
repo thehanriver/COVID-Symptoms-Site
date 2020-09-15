13fbab678d847dd223e44e770f9c15b30a010d49
Kercho0 <70703656+Kercho0@users.noreply.github.com>
2020-09-14 20:03:34 -0400

    Add files via upload

--- src/Pages/AdminDash.js
+++ src/Pages/AdminDash.js
@@ -4,8 +4,11 @@ export default class AdminDash extends Component {
 	render() {
 		return (	
 			<div className="AdminDash">
-          <h1>admin dash</h1>
-          <p> A Covid app for EC-463 </p>
+          <h1>admin dashboard</h1>
+          <p> Reported Symptoms Data Visualization </p>
+          <iframe title = "Bar Graph" width="1008" height="622" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4aRX7-AdEJh3N8zZ5O_-U6-7PjL0D01pXw9lCXZYWg1BrwHRQBbYIw3vifPzPpCQrzeGbiC1L7Rn7/pubchart?oid=1422167781&amp;format=interactive"></iframe>
+          <iframe title = "Pie Chart" width="600" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4aRX7-AdEJh3N8zZ5O_-U6-7PjL0D01pXw9lCXZYWg1BrwHRQBbYIw3vifPzPpCQrzeGbiC1L7Rn7/pubchart?oid=1766034756&amp;format=interactive"></iframe>
+
       		</div>
 			);
 		}
