8ded02e5987b18658eb576b1552caf5330a86ab4
marioisgay123 <hanmario@bu.edu>
2020-09-15 14:08:30 -0400

    settings option added

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -1,66 +1,117 @@
-import React, {Component } from "react";
+import React, { useState } from "react";
 import firebase from 'firebase';
 
+const PasswordReset = () => {
 
-export default class Settings extends Component {
- 	
- 	constructor(props) {
- 		super(props);
- 		this.state = {
- 		formControls: {
- 		email: {value:''},
- 		displayName: {value:''}
- 		}}
- 	}
- 	
- 	handlePass = event =>{
- 		const name = event.target.name;
- 		const value = event.target.value;
+ 	const [email, setEmail] = useState("");
+	const [emailHasBeenSent, setEmailHasBeenSent] = useState(false);
+	const [error, setError] = useState(null);
+	const [dispName, setDisplayName] = useState("");
+	const [displayNameChange, setDisplayNameChange] = useState(false);
+	const [dispErr, setdispError] = useState(null);
 
- 		this.setState({
- 			formControls: {
- 				...this.state.formControls,[name]: {...this.stateformControls[name]},value}
- 		});
- 	}
+	const onChangeHandler = (event) => {
+	const {name,value} = event.currentTarget;
+		if(name === "userEmail"){
+			setEmail(value);
+		}
+		else if(name === "dispName"){
+			setDisplayName(value);
+		}
 
- 	
-	
+	};
 
-	render() {
-		const email =' ';
-		return (	
+	const sendResetEmail = (event) =>{
+		event.preventDefault();
+		firebase.auth()
+		.sendPasswordResetEmail(email)
+		.then(() => {
+			setEmailHasBeenSent(true);
+			setTimeout(()=>{setEmailHasBeenSent(false)},3000);
+		})
+		.catch(()=>{
+		setError("Error resetting password");
+		});
+	};
+
+	const resetDisplayName = (event) =>{
+		event.preventDefault();
+		setDisplayName(event.target.value);
+		
+		firebase.auth()
+		.onAuthStateChanged(currentUser => {
+		firebase.auth()
+		.currentUser
+		.updateProfile({
+			displayName : dispName
+		})
+		.then(() => {
+			setDisplayNameChange(true);
+			setTimeout(()=>{setDisplayNameChange(false)},3000);
+		})
+		.catch(()=>{
+		setdispError("Error changing Display Name");
+		});
+		})
+	};
+
+	return (	
 			<div className="Settings">
           		<h1>Settings</h1>
-          			<h2> Change your password (work in progress) </h2>
-          			<input
-          			 className = "Email"
-            		 type="text"
-            		 placeholder = "Input your email"
-           			 onChange={this.state.formControls.email.value}
-        			 />
-        			 <button
-        			 className = "" 
-        			 onClick = {event => {firebase.auth().currentUser.sendPasswordResetEmail(this.state.formControls.email.value).then(function(){alert('Check your Email')}).catch(function(err){console.log(event)})}}
-        			 >
-        			 Send Reset Password
-        			 </button>
+          			<div>
+          				<h2>Change your password</h2>
+						<form action ="">
+							{emailHasBeenSent && (<div>Email has been sent!</div>)}
+								{error !== null && (<div>{error}</div>)}
+										<label htmlFor ="userEmail" className ="w-full block">
+											Send reset link to:  
+										</label>
 
-          			<h2> Change your Display Name</h2>
-          			<input
-          			 className = "Email"
-            		 type="text"
-            		 placeholder = "Input your new name"
-           			 onChange={this.state.formControls.email.value}
-        			 />
-        			 <button
-        			 className = "" 
-        			 onClick = {event => {firebase.auth().currentUser.sendPasswordResetEmail(this.state.formControls.email.value).then(function(){alert('Check your Email')}).catch(function(err){console.log(event)})}}
-        			 >
-        			 Change Display Name
-        			 </button>
-      		
+								<input
+									type ="email"
+									name= "userEmail"
+									id="userEmail"
+									value={email}
+									placeholder="Input your email"
+									onChange ={event => {onChangeHandler(event)}}
+									
+								/>
+								<button
+									onClick = {event => {
+										sendResetEmail(event);
+									}}
+								>
+								 Send me a reset link</button>
+						</form>
+					</div>
+					<div>
+						<h2>Change your display name </h2>
+						<form action ="">
+							{displayNameChange && (<div>Display Name changed!</div>)}
+								{dispErr !== null && (<div>{dispErr}</div>)}
+									<label htmlFor ="displayName" className ="w-full block">
+										Display Name: 
+										</label>
+
+								<input
+									type ="dispName"
+									name= "dispName"
+									id="dispName"
+									value={dispName}
+									placeholder="Input your name"
+									onChange ={event => {onChangeHandler(event)}}
+								/>
+								<button
+									onClick = {event => {resetDisplayName(event);						
+									}}
+								>
+								 Change my display name</button>
+						</form>
+					</div>
       		</div>
 			);
-		}
-}
-   
+		
+
+};
+
+export default PasswordReset;
