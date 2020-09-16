86ee0bc6d86796cb2dd61b631e335b3561c9493f
Kercho0 <70703656+Kercho0@users.noreply.github.com>
2020-09-16 13:52:32 -0400

    Add files via upload

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,6 @@ import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import Survey from "./Pages/Survey";
 import AdminDash from "./Pages/AdminDash";
-import Settings from "./Pages/Settings";
 
 export default class Routes extends Component {
 	render() {
@@ -17,7 +16,6 @@ export default class Routes extends Component {
 				<Route path ="/Login" component ={Login}/>
 				<Route path ="/Survey" component ={Survey}/>
 				<Route path ="/AdminDash" component ={AdminDash}/>
-				<Route path = "/Settings" component = {Settings}/>
 			</Switch>
 			</Router>
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
   
+
 ReactDOM.render(
   <Router>
-   	<App />
+  <App />
    </Router>,
   
   document.getElementById('root')
